363b4bfdd047e42280265f80d1ea6959f20512b4
SofiaRoumieu <roumieusofia@gmail.com>
2020-08-30 19:10:28 -0300

    nuevo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PruebaHeroku';
+  title = 'my-first-project';
+
+  public numero1 ;
+  public numero2;
+  public promedio;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  Promediar() {
+    this.promedio = (this.numero1+this.numero2)/2;
+  }
+
+  Limpiar(){
+    this.numero1='';
+    this.numero2='';
+    this.promedio='';
+  }
 }
