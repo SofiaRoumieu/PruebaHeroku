b058e6ded26fc6839012e506b6b4268021bf3d87
SofiaRoumieu <roumieusofia@gmail.com>
2020-08-31 22:31:12 -0300

    Agrego componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Componentes/login/login.component';
+import { BienvenidoComponent } from './Componentes/bienvenido/bienvenido.component';
+import { ErrorComponent } from './Componentes/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    BienvenidoComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
